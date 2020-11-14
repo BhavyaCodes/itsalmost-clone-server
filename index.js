08234b949b8bbf4020e88574592081177c3960b9
Matthew Fee <matthewefee@gmail.com>
2020-11-14 13:17:57 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi there");
+
+
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT)
+
+console.log("listening on port 5000...");

87b8358c59155830ae9a738b3ca5ec9bae4eae83
Bryan Urizar <bryan.urizar@outlook.com>
2020-11-14 23:29:52 +0000

    Add mongoose connection / install dotenv

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
 
-
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to DB'))
+    .catch((err) => console.error(err));
+
+
+const postSchema = new mongoose.Schema({
+    eventName: String,
+    eventDate: Date
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 app.listen(PORT)
 

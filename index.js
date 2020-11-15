c2556dec79de28932b5c88f725c2e715e6204b76
Bryan Urizar <bryan.urizar@outlook.com>
2020-11-15 09:48:56 +0000

    make values required

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
 const postSchema = new mongoose.Schema({
-    eventName: String,
-    eventDate: Date
+    eventName: {
+        type: String,
+        required: true
+    },
+    eventDate: {
+        type: Date,
+        required: true
+    }
 });
 
 const Post = mongoose.model('Post', postSchema);

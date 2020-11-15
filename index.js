4aaa568f5821bab2582e412d28bd2a427e9c571c
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-15 15:43:12 +0530

    add /ping route for testing

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to DB'))
-    .catch((err) => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error(err));
 
+//mongoose model
 
 const postSchema = new mongoose.Schema({
-    eventName: {
-        type: String,
-        required: true
-    },
-    eventDate: {
-        type: Date,
-        required: true
-    }
+  eventName: {
+    type: String,
+    required: true,
+  },
+  eventDate: {
+    type: Date,
+    required: true,
+  },
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 
-app.listen(PORT)
+// routes
 
-console.log("listening on port 5000...");
+app.get("/ping", (req, res, next) => {
+  res.send({ ping: "pong" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});

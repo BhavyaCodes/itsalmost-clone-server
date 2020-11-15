5810ac48246bebd039fff98354563d2fc8ff3bbc
Bryan Urizar <73317691+bryanurizar@users.noreply.github.com>
2020-11-15 09:51:53 +0000

    Add mongoose connection / install dotenv (#7)
    * Add mongoose connection / install dotenv
    
    * make values required

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
 
-
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to DB'))
+    .catch((err) => console.error(err));
+
+
+const postSchema = new mongoose.Schema({
+    eventName: {
+        type: String,
+        required: true
+    },
+    eventDate: {
+        type: Date,
+        required: true
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 app.listen(PORT)
 
